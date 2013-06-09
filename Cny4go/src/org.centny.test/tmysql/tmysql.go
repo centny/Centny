2d546f80fb32f9e464751c69a4824b4cffae1b7d
Centny <centny@gmail.com>
2013-06-09 10:27:49 +0800

    update test for mysql.

--- Cny4go/src/org.centny.test/tmysql/tmysql.go
+++ Cny4go/src/org.centny.test/tmysql/tmysql.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	fmt.Println("aaaa")
+	// fmt.Println("aaaa")
+	db, e := sql.Open("mysql", "root:sco@unix(/tmp/mysql.sock)/test")
+	if e != nil {
+		fmt.Println("open database error", e)
+		return
+	}
+	rows, err := db.Query("SELECT * FROM TNAME")
+	if err != nil {
+		fmt.Println("query error", err)
+		return
+	}
+	for rows.Next() {
+		var aid, bid string
+		if err := rows.Scan(&aid, &bid); err != nil {
+			fmt.Println("err:", err)
+		} else {
+			fmt.Printf("aid:%s,bid:%s\n", aid, bid)
+		}
+	}
+	db.Close()
+	// fmt.Println("rows", rows)
 }
