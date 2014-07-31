4cd6e8fdfe061da228073ba0f38d2e25ac6cab39
Centny <centny@gmail.com>
2014-07-31 10:54:30 +0800

    adding test code for nodes

--- TestNode.js/src/node/test.js
+++ TestNode.js/src/node/test.js
@@ -1,29 +1,20 @@
-//var jsdom = require("jsdom");
-//var window = jsdom.jsdom().parentWindow;
-//require('jquery')(window);
-"use strict";
-var jsdom = require("jsdom");
-global.window = jsdom.jsdom().parentWindow;
-//XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var $ = require("jquery")(window)
-//console.log(window)
-$.get("http://www.baidu.com",function(d){
-  console.log(d);
-},function(e){
-  console.log(e);
-});
-var http = require("http");  
-  
-var url = require("url");  
-function onRequest(request,response){  
-      var pathname = url.parse(request.url).pathname;  
-      console.log("Request for " + pathname + "  received.") ;  
+(function () {
+  'use strict';
 
-      route(pathname);  
+  var env = require('jsdom').env
+    , html = '<html><body><h1>Hello World!</h1><p class="hello">Heya Big World!</body></html>'
+    ;
 
-      response.writeHead(200,{"Content-Type":"text/plain"});  
-      response.write("Hello World");  
-      response.end();  
-  }  
+  // first argument can be html string, filename, or url
+  env(html, function (errors, window) {
+    console.log(errors);
 
-http.createServer(onRequest).listen(8080);  
+    var $ = require('jquery')(window)
+      ;
+
+    console.log($('.hello').text());
+    $.get("http://www.baidu.com",{},function(a){
+    	console.log(a);
+    })
+  });
+}());
